4457c1fd77234c0dc6ad7d37f875649b8c3b284b
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-04-03 02:37:15 +0800

    Initial

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public')); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended:true }));
+
+
+//Routes
+const introRoute = require('./routes/intro');
+
+//Routes - Pages
+app.use('/',        introRoute);
+app.engine("hbs", exphbs.engine({extname: 'hbs'}));
+app.set("view engine", "hbs")
+app.set("views", "./views")
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {console.log(`Server listening on port ${port}`)});
+
+
+app.use((req,res,next)=>{
+  console.log(`${req.method}:${req.url}`)
+  next();
+})
+
---
+++ public/scripts/intro.js
@@ -0,0 +1,45 @@
+const proxmoxUsername = 'stadvdb36';
+const proxmoxPassword = 'w5EuLsQ8WHk2XyfJaZhSNen4';
+
+$(document).ready(function () {
+    performActions(proxmoxUsername, proxmoxPassword);
+});
+
+function authenticate(username, password, callback) {
+    // Make a POST request to /api2/json/access/ticket endpoint to authenticate
+    $.ajax({
+        url: '/api2/json/access/ticket',
+        type: 'POST',
+        contentType: 'application/x-www-form-urlencoded',
+        data: {
+            username: username,
+            password: password
+        },
+        success: function(response) {
+            var csrfToken = response.data.CSRFPreventionToken;
+            callback(csrfToken);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error authenticating with Proxmox API:', error);
+        }
+    });
+}
+
+function performActions(username, password) {
+    authenticate(username, password, function(csrfToken) {
+        $.ajax({
+            url: '/api2/json/nodes',
+            type: 'GET',
+            headers: {
+                'CSRF-Token': csrfToken
+            },
+            success: function(response) {
+                console.log('Successfully communicated with the online server.');
+                console.log('Proxmox Node Information:', response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Error getting Proxmox node information:', error);
+            }
+        });
+    });
+}
