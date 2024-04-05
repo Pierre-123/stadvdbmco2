9e5dc975424192a3126164ccea133ca96332a6d8
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-04-05 12:59:20 +0800

    Test Button
    Tried making it a button, still not sure how to connect it properly.

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,14 +1,18 @@
 const proxmoxUsername = 'stadvdb36';
 const proxmoxPassword = 'w5EuLsQ8WHk2XyfJaZhSNen4';
+const test_button = $("#test-btn");
 
 $(document).ready(function () {
-    performActions(proxmoxUsername, proxmoxPassword);
+    test_button.click(function(){
+        performActions(proxmoxUsername, proxmoxPassword);
+        console.log("Actions Performed")
+    });
 });
 
 function authenticate(username, password, callback) {
     // Make a POST request to /api2/json/access/ticket endpoint to authenticate
     $.ajax({
-        url: '/api2/json/access/ticket',
+        url: 'https://ccscloud.dlsu.edu.ph:20108/api2/json/access/ticket',
         type: 'POST',
         contentType: 'application/x-www-form-urlencoded',
         data: {
--- routes/intro.js
+++ routes/intro.js
@@ -1,8 +1,30 @@
 const { Router } = require('express'); 
 const router = Router();
+const axios = require('axios');
 
 router.get('/', (req, res) => {
     res.render('intro', {title: "A Page"});
 });
+/*
+router.post('/api2/json/access/ticket', async (req, res) => {
+    try {
+        // Extract username and password from request body
+        const { username, password } = req.body;
+
+        // Make a POST request to authenticate with Proxmox API
+        const response = await axios.post('https://ccscloud.dlsu.edu.ph:20108/api2/json/access/ticket', {
+            username: username,
+            password: password
+        });
+
+        // Return the response from Proxmox API to the client
+        res.json(response.data);
+    } catch (error) {
+        // If an error occurs, return an error response
+        console.error('Error authenticating with Proxmox API:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});*/
+
 
 module.exports = router;
