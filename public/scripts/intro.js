9b3ddac33b2d7bb2fafff3d617baece9bc0e44c7
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-04-09 16:59:30 +0800

    Trying to connect to proxmox, still not there.
    It's logging the html page not the actual node data. I'm missing something still.

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,56 +1,20 @@
-const proxmoxUsername = 'stadvdb36';
-const proxmoxPassword = 'w5EuLsQ8WHk2XyfJaZhSNen4';
+
+const proxmoxProxy = 'http://localhost:3000/proxy';
 const test_button = $("#test-btn");
 
 $(document).ready(function () {
     test_button.click(function(){
-        performActions(proxmoxUsername, proxmoxPassword);
+        connect();
         console.log("Actions Performed")
     });
 });
 
- function authenticate(username, password, callback) {
-    // Make a POST request to /api2/json/access/ticket endpoint to authenticate
-    $.ajax({
-        url: 'https://ccscloud.dlsu.edu.ph:20108/api2/json/access/ticket',
-        type: 'POST',
-        contentType: 'application/x-www-form-urlencoded',
-        data: {
-            username: username,
-            password: password
-        },
-        success: function(response) {
-            var csrfToken = response.data.CSRFPreventionToken;
-            callback(csrfToken);
-        },
-        error: function(xhr, status, error) {
-            console.error('Error authenticating with Proxmox API:', error);
-        }
-    });
-}
-
-function performActions(username, password) {
-    authenticate(username, password, function(csrfToken) {
-        $.ajax({
-            url: '/api2/json/nodes',
-            type: 'GET',
-            headers: {
-                'CSRF-Token': csrfToken
-            },
-            success: function(response) {
-                console.log('Successfully communicated with the online server.');
-                console.log('Proxmox Node Information:', response);
-            },
-            error: function(xhr, status, error) {
-                console.error('Error getting Proxmox node information:', error);
-            }
-        });
-    });
-}
-
-async function fetchPerformAction(username, password) {
-    const response = await fetch('/api2/json/nodes', {
-        method: "POST",
-        mode: "cors",
-    }) 
+async function connect(){
+    try {
+        const response = await fetch(proxmoxProxy);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
--- routes/intro.js
+++ routes/intro.js
@@ -1,30 +1,39 @@
 const { Router } = require('express'); 
 const router = Router();
 const axios = require('axios');
+const https = require('https');
+const Proxmox = require('proxmox');//https://www.npmjs.com/package/proxmox
+const { time } = require('console');
+const { hostname } = require('os');
+const username = 'stadvdb36';
+const password = 'w5EuLsQ8WHk2XyfJaZhSNen4';
+
+const Proxmox_Server0 = new Proxmox({
+    hostname: 'STADVDB36-Server0',
+    username: 'stadvdb36',
+    password: 'w5EuLsQ8WHk2XyfJaZhSNen4'
+})
+
 
 router.get('/', (req, res) => {
     res.render('intro', {title: "A Page"});
 });
-/*
-router.post('/api2/json/access/ticket', async (req, res) => {
-    try {
-        // Extract username and password from request body
-        const { username, password } = req.body;
 
-        // Make a POST request to authenticate with Proxmox API
-        const response = await axios.post('https://ccscloud.dlsu.edu.ph:20108/api2/json/access/ticket', {
+router.get('/proxy', async (req, res) => {
+    try { //getting https://ccscloud.dlsu.edu.ph/ gets the html page. adding the :20108 ERCONNRefuses/ETIMEDOUT hmm. 
+      const response = await axios.get('https://ccscloud.dlsu.edu.ph', {
+        httpsAgent: new https.Agent({ rejectUnauthorized: false}), 
+        auth: {
             username: username,
             password: password
-        });
-
-        // Return the response from Proxmox API to the client
-        res.json(response.data);
+        }
+      });
+      console.log('What: ', response.data)
+      res.json(response.data);
     } catch (error) {
-        // If an error occurs, return an error response
-        console.error('Error authenticating with Proxmox API:', error);
-        res.status(500).json({ error: 'Internal server error' });
+      console.error('Error fetching data:', error);
+      res.status(500);
     }
-});*/
-
+  });
 
 module.exports = router;
