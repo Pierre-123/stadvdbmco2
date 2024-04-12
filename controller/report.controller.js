cd2ad6156b28a2b5e39adaad6b3aee5f02a8947a
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-04-12 20:54:02 +0800

    report amount of appointments

--- controller/report.controller.js
+++ controller/report.controller.js
@@ -1,5 +1,22 @@
 const pools = require('../conn/dbService')
 
 const reportController = {
-
+    getAmountAppointments: async () => {
+        const [rows, field] = await pools.centralPool2.query(`
+               SELECT COUNT(apptid) as amounta
+               FROM VisMin
+               `);
+               const [rows2, field2] = await pools.centralPool.query(`
+               SELECT COUNT(apptid) as amountb 
+               FROM Luzon
+               `)
+               const jsonrows = JSON.parse(JSON.stringify(rows))
+               const jsonrows2 = JSON.parse(JSON.stringify(rows2))
+               console.log(jsonrows[0].amounta)
+               console.log(jsonrows2[0].amountb)
+               const row_processed = jsonrows[0].amounta + jsonrows2[0].amountb;
+               return row_processed
+   }
 }
+
+module.exports = reportController
--- routes/report.js
+++ routes/report.js
@@ -1,11 +1,17 @@
 const { Router, json } = require('express'); 
 const router = Router();
 const indexController = require('../controller/index.controller')
+const reportController = require('../controller/report.controller')
 
 router.get('/', async (req, res) => {
     try {
       console.log("put report here")
-      res.render('report', {title: "Report"})
+      const nAppointments = await reportController.getAmountAppointments();
+      const cAppointments = 0;
+      const nDPatients = 0;
+      const nDDoctors = 0;
+
+      res.render('report', {title: "Report", nAppointments: nAppointments, cAppointments: cAppointments, nDPatients: nDPatients, nDDoctors:nDDoctors})
     } catch (err) {
       console.log(err)
       res.render('report', {title: "Report"})
